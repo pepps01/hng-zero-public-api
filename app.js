85b5389ee79a17a0d222dae1daccd2fd555ce735
pepps01 <slpepple01@gmail.com>
2025-02-09 17:54:02 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+const port = process.env.PORT || 3000
+ 
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello, world!',
+  })
+})
+ 
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`)
+})
